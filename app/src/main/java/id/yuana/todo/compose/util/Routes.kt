2effed38b4d33242c3f4918cb4a8c64b706da776
andhikayuana <andhikayuana@gmail.com>
2022-12-16 21:04:50 +0700

    chore: refactor routes to screen for better navigation

--- app/src/main/java/id/yuana/todo/compose/util/Routes.kt
+++ app/src/main/java/id/yuana/todo/compose/util/Routes.kt
@@ -1,5 +1,6 @@
 package id.yuana.todo.compose.util
 
+@Deprecated(message = "please use id.yuana.todo.compose.navigation.Screen instead")
 object Routes {
     const val TODO_LIST = "todo_list"
     const val TODO_ADD_EDIT = "todo_add_edit"
