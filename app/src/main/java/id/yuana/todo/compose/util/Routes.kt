05c465d3fdeb15670b63802ac38612d10bc783ae
andhikayuana <andhikayuana@gmail.com>
2023-01-02 15:18:40 +0700

    chore: delete deprecated file

--- app/src/main/java/id/yuana/todo/compose/util/Routes.kt
+++
@@ -1,9 +0,0 @@
-package id.yuana.todo.compose.util
-
-@Deprecated(message = "please use id.yuana.todo.compose.navigation.Screen instead")
-object Routes {
-    const val TODO_LIST = "todo_list"
-    const val TODO_ADD_EDIT = "todo_add_edit"
-    const val LOGIN = "login"
-    const val REGISTER = "register"
-}
